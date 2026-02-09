14800839b32ba5c453ebbffc9af7eec2f5b53555
ryanwilliamdev <rywilldav@gmail.com>
2026-02-09 13:23:34 -0500

    Added top terpenes objects to server.js

--- server.js
+++ server.js
@@ -2,8 +2,47 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const terpeneData = {
+  topTerpenes: [
+    {
+      id: 1,
+      name: "Myrcene",
+      aroma: "Earthy, Musky, Cloves",
+      effects: ["Relaxing", "Sedating"],
+    },
+    {
+      id: 2,
+      name: "Limonene",
+      aroma: "Citrus, Lemon, Orange",
+      effects: ["Mood Elevation", "Stress Relief"],
+    },
+    {
+      id: 3,
+      name: "Caryophyllene",
+      aroma: "Peppery, Spicy, Wood",
+      effects: ["Anti-inflammatory", "Anxiety Relief"],
+    },
+    {
+      id: 4,
+      name: "Linalool",
+      aroma: "Floral, Lavender",
+      effects: ["Calming", "Sleep Aid"],
+    },
+    {
+      id: 5,
+      name: "Pinene",
+      aroma: "Pine, Sharp, Sweet",
+      effects: ["Alertness", "Memory Retention"],
+    }
+  ]
+}
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api', (request, response) => {
+    response.json()
 })
 
 app.listen(PORT, () => {
