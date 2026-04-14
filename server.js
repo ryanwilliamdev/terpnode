d5e240903365afc0259470dae5e9f841cc218084
raindevice <rywilldav@gmail.com>
2026-04-13 21:08:11 -0400

    update server.js. connection to database working now.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 // Load config
-dotenv.config({ path: './config' })
+dotenv.config({ path: './config/config.env' })
+
+
 
 const app = express()
 
@@ -12,6 +15,7 @@ app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
+connectDB()
 
 
 
