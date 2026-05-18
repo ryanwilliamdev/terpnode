72095ee54c8de40bbc00a509b750dddcca68d17e
ryanwilliamdev <rywilldav@gmail.com>
2026-05-18 15:00:07 -0400

    add toast notification button in App component for user feedback

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import StrainDetailPage from './pages/StrainDetailPage'
+import toast from 'react-hot-toast'
 
 const App = () => {
   return (
     <div>
+      <button onClick={() => toast.error('Success!')}>Click me!</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
