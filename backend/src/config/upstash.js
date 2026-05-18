71d4400478861f869443ac61830904785f4face5
raindevice <rywilldav@gmail.com>
2026-05-17 21:46:06 -0400

    update ratelimit constructor

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,4 +1,4 @@
-import { RateLimit } from '@upstash/ratelimit';
+import { Ratelimit } from '@upstash/ratelimit';
 import { Redis } from '@upstash/redis';
 
 import dotenv from 'dotenv';
@@ -8,10 +8,7 @@ dotenv.config();
 // create a rate limit instance with a sliding window of 20 seconds and a limit of 10 requests
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Redis.slidingWindow({
-        interval: '1m', // 1 minute
-        limit: 100, // 100 requests
-    }),
+    limiter: Ratelimit.slidingWindow(100, '60 s'),
 });
 
 export default ratelimit;
