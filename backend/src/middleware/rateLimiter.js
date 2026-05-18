b208d24ed3a3bd3888972c1dae383b7617b64cc2
raindevice <rywilldav@gmail.com>
2026-05-17 21:27:21 -0400

    add rateLimiter async function for upstash

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,16 @@
+import ratelimit from '../config/upstash.js';
+
+const rateLimiter = async (req, res, next) => {
+    try {
+        const { success } = await ratelimit.limit(req.ip);
+        if (!success) {
+            return res.status(429).json({ message: 'Too many requests' });
+        }
+        next();
+    } catch (error) {
+        console.error('Error in rateLimiter middleware:', error);
+        return res.status(429).json({ message: 'Too many requests' });
+    }
+};
+
+export default rateLimiter;
