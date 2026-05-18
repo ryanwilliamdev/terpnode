ab882d8ec923ab2e1bd548d1b0db6291b6a5d776
ryanwilliamdev <rywilldav@gmail.com>
2026-05-18 15:14:07 -0400

    remove toast notification button from App component and adjust formatting in JSX

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage'
-import CreatePage from './pages/CreatePage'
-import StrainDetailPage from './pages/StrainDetailPage'
-import toast from 'react-hot-toast'
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import StrainDetailPage from './pages/StrainDetailPage';
 
 const App = () => {
   return (
     <div>
-      <button onClick={() => toast.error('Success!')}>Click me!</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/strain/:id" element={<StrainDetailPage />} />
       </Routes>
-    </div>
+    </div >
   )
 }
 
