c7535edfd80a5ec4894ddd0f788a1bc74ee4e85f
ryanwilliamdev <rywilldav@gmail.com>
2026-05-19 14:08:38 -0400

    update HomePage component to include Navbar and apply minimum height styling

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-        <div>HomePage</div>
-    )
-}
+        <div className='min-h-screen'>
+            <Navbar />
+        </div>
+    );
+};
 
 export default HomePage
